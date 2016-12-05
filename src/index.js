ef0d506f332f07b8add09bb3cd21f141d139b674
Krzysztof Kraszewski <krszwsk@gmail.com>
2016-12-05 17:44:42 +0100

    :zap: :sparkles: showing hex and rgb, calculating brightness to change font color

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
-var _gaq = _gaq || []
-_gaq.push(['_setAccount', 'UA-88337677-2'])
-_gaq.push(['_trackPageview'])
+handler()
 
-(function() {
-  var ga = document.createElement('script')
-  ga.type = 'text/javascript'; ga.async = true
-  ga.src = 'https://ssl.google-analytics.com/ga.js'
-  var s = document.getElementsByTagName('script')[0]
-  s.parentNode.insertBefore(ga, s)
-})()
+function handler () {
+  var hex = generateColour()
+  var rgb = hexToRgb(hex)
+  var luma = getLuma(rgb)
 
+  document.getElementById('hex').innerHTML = hex
+  document.getElementById('rgb').innerHTML = rgb.rgb
+  document.getElementById('luma').innerHTML = luma
+
+  document.body.style.backgroundColor = hex
+  luma < 120 ? document.body.style.color = 'white' : document.body.style.color = 'black'
+}
+
+function generateColour () {
+  return '#' + Math.random().toString(16).slice(2, 8)
+}
+
+function hexToRgb(hex) {
+  hex = hex.substring(1)
+  var dec = parseInt(hex, 16)
+  var rgb = {
+    r: (dec >> 16) & 0xff,
+    g: (dec >> 8) & 0xff,
+    b: (dec >> 0) & 0xff
+  }
+  rgb.rgb = 'rgb(' + rgb.r + ', ' + rgb.g + ', ' + rgb.b + ')'
+  return rgb
+}
+
+function getLuma(rgb) {
+  return 0.2126 * rgb.r + 0.7152 * rgb.g + 0.0722 * rgb.b
+}
